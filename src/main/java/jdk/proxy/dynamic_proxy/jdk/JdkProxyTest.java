f1913ec6c3067ff3253ecd030688ef788c4ce395
wang <42605435+wingwyj@users.noreply.github.com>
2022-03-07 10:17:36 +0800

    Update JdkProxyTest.java
    jdk动态代理注解

--- src/main/java/jdk/proxy/dynamic_proxy/jdk/JdkProxyTest.java
+++ src/main/java/jdk/proxy/dynamic_proxy/jdk/JdkProxyTest.java
@@ -9,7 +9,23 @@ import jdk.proxy.SmsServiceImpl;
  */
 public class JdkProxyTest {
     public static void main(String[] args) {
+        // 通过该方法设置可以查看动态生成的代理类
+        // System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\develop\\noteTip\\src\\main\\java\\com\\notetip\\test\\jdk\\dynamicproxy\\cglib\\");
+        
         SmsService smsService = (SmsService)JdkProxyFactory.getProxy(new SmsServiceImpl());
         smsService.send("JDK动态代理发送短信");
+        
+        // new JdkProxyTest().generateProxy();
+    }
+    
+    public void generateProxy() {
+        String name = "SmServiceImplProxy";
+        byte[] bytes = ProxyGenerator.generateProxyClass(
+                name, new Class[]{SmService.class});
+        try (FileOutputStream out = new FileOutputStream(name + ".class");) {
+            out.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
